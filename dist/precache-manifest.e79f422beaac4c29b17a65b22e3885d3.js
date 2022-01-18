d77b1129b5360d9ef8539ee24dfe036ae38291be
Lutsenko Alexsey <Lutsenko_AE@NSKGK.LAN>
2022-01-18 17:00:23 +0700

    first commit

---
+++ dist/precache-manifest.e79f422beaac4c29b17a65b22e3885d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e1dabc7d17c4ae2ee4",
+    "url": "/css/app.37adbbe3.css"
+  },
+  {
+    "revision": "ba0200460055c3222fff32e4a2d5760a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3e1dabc7d17c4ae2ee4",
+    "url": "/js/app.dacdb921.js"
+  },
+  {
+    "revision": "264ed49e10ad044ba389",
+    "url": "/js/chunk-2d0a4783.8ba4cf15.js"
+  },
+  {
+    "revision": "f74d996253593e0ac8eb",
+    "url": "/js/chunk-2d0be70b.a24ea852.js"
+  },
+  {
+    "revision": "5945aa3aebd6e5afff4b",
+    "url": "/js/chunk-2d216dc7.2d299929.js"
+  },
+  {
+    "revision": "29db97d6d98342c883b1",
+    "url": "/js/chunk-vendors.1aab9bc5.js"
+  },
+  {
+    "revision": "f11bce428d38fd9f4d6f0ba31c2acffc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="layout + '-layout'" v-if="layout"></component>
+<!--  <main-layout></main-layout>-->
+<!--  <auth-layout></auth-layout>-->
+</template>
+
+<script>
+import {computed} from 'vue'
+import {useRoute} from 'vue-router'
+import AuthLayout from "./layout/AuthLayout";
+import MainLayout from "./layout/MainLayout";
+
+export default {
+  setup() {
+    const route = useRoute()
+    return {
+      layout: computed(() => route.meta.layout)
+    }
+  },
+  components: {AuthLayout, MainLayout}
+}
+</script>
